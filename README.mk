2fdadc14affc2f5725f4883a7a3b94cfb3c9b5b6
your name <example@yahoo.com>
2018-02-07 12:42:39 -0600

    readme

--- README.mk
+++ README.mk
@@ -1,3 +1,13 @@
 Repositorio creado para proyecto de puesto Semi sr desarrollador python
 el folder directorio contiene el codigo de backend en pytho+django-rest-framework
 el frontend esta el codigo javascript + react js
+
+para el codigo de django desde la carpeta directorio y con virtualenv creado se ejecuta lo siguiente:
+	pip install -r requirements.txt
+
+cuando termina de instalar se ejecuta:
+	python manage.py runserver
+
+para el frontend en la carpeta frontend/build/ hay un archivo index.html, se ejecuta y ese es la app
+
+
