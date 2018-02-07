a5a2bbd06df87e3d066f0ea2c75802c20f139f3f
your name <example@yahoo.com>
2018-02-07 12:38:53 -0600

    readme

---
+++ README.mk
@@ -0,0 +1,3 @@
+Repositorio creado para proyecto de puesto Semi sr desarrollador python
+el folder directorio contiene el codigo de backend en pytho+django-rest-framework
+el frontend esta el codigo javascript + react js
